28ab7009092c1bafa04061e5fcb7af2ca34bdd06
Fidel Ramos <f@fidelramos.net>
2020-07-14 12:58:38 +0200

    Move decode

--- onlykey_agent/formats.py
+++ onlykey_agent/formats.py
@@ -179,8 +179,8 @@ def export_public_key(vk, label):
     """
     key_type, blob = serialize_verifying_key(vk)
     log.debug('fingerprint: %s', fingerprint(blob))
-    b64 = base64.b64encode(blob).decode('ascii')
-    return '{} {} {}\n'.format(key_type.decode('ascii'), b64, label)
+    b64 = base64.b64encode(blob)
+    return '{} {} {}\n'.format(key_type.decode('ascii'), b64.decode('ascii'), label)
 
 
 def import_public_key(line):
